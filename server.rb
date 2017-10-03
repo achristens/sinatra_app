907b90a3fbacf79571265e7a3c237a2f9994cc68
Abby Christens <achristens@gmail.com>
2017-10-03 16:16:54 -0400

    Adding favourite links

--- server.rb
+++ server.rb
@@ -9,5 +9,18 @@ get '/portfolio' do
 end
 
 get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about
+end
+
+get '/favourites' do
+  @fav_links = [
+    'http://underconsideration.com/',
+    'https://sidebar.io/',
+    'https://medium.com/',
+    'https://www.reddit.com/r/AsianBeauty/',
+    'https://www.houseplantjournal.com/'
+  ]
+  erb :favourites
 end
